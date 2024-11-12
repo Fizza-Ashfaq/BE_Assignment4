cad952bac4d39f32d68adf7fe2e64e31e43280a1
Fizza-Ashfaq <fizza0849@gmail.com>
2024-11-12 12:14:22 +0500

    implemented create method

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 const express=require('express');
 const mongoose=require('mongoose');
-const {schemas}=require('mongoose');
+const {Schema}=require('mongoose');
 const app=express();
 app.use(express.json());
 
-mongoose.connect('mongodb://localhost/mydatabase').then(()=>console.log('mongodb'));
+mongoose.connect('mongodb://localhost:27017/books').then(()=>console.log('mongodb'));
 const bookSchema=new Schema({
     title:{type:String, required:true},
     author:{type:String, required:true},
@@ -12,3 +12,13 @@ const bookSchema=new Schema({
     genre:{type:String, required:true},
     summary:{type:String, required:false},
 });
+const Book=mongoose.model('Book',bookSchema);
+app.post('/books',async(req,res)=>
+{
+    const data=req.body;
+    const obj=await Book.create(data);
+    res.json(obj);
+
+})
+const port=8000;
+app.listen(port);
