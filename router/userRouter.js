bde375ae11ffc0e65364b6887b03e287601966d8
Fizza-Ashfaq <fizza0849@gmail.com>
2024-11-22 19:56:40 +0500

    Added routers for both book and user

---
+++ router/userRouter.js
@@ -0,0 +1,10 @@
+const express=require('express');
+const router=express.Router();
+const controller=require('../controller/userController');
+router.post('/users',controller.adduser);
+router.get('/getusers',controller.getuser);
+router.get('/getusersbyid/:id',controller.getoneuser);
+router.put('/updateusers/:id',controller.updateuser);
+router.delete('/deleteusers/:id',controller.deleteuser);
+
+module.exports=router;
